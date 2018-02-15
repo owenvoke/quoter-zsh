e648fd4247eb70de5dbe0f8a684fc1b47244c564
pxgamer <owzie123@gmail.com>
2018-02-15 12:34:16 +0000

    Add quoter plugin script

---
+++ quoter-zsh.plugin.zsh
@@ -0,0 +1,15 @@
+# Set a custom quote bank file using the QUOTER_BANK variable
+if [[ ! "${QUOTER_BANK}" ]]; then
+    QUOTER_BANK = "default"
+fi
+
+# Check if quote bank file exists
+if [[ -f ${QUOTER_BANK} ]]
+    # Include the quote bank file
+    source "./src/${QUOTER_BANK}.zsh"
+
+	# Output a random quote
+    echo "${quoter_selection[RANDOM % #quoter_selection + 1]}"
+	echo
+	unset quoter_selection
+fi
