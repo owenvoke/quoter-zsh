1264dfe304571b08f6fac736c450c751945cd8d4
pxgamer <owzie123@gmail.com>
2018-02-16 11:21:18 +0000

    Add some actual quotes (by Linus Torvalds)

--- src/default.zsh
+++ src/default.zsh
@@ -1,3 +1,6 @@
 quoter_selection=(
-	"A boring test quote."
+	"Intelligence is the ability to avoid doing work, yet getting the work done.\n - Linus Torvalds"
+	"People who are doing things for fun do things the right way by themselves.\n - Linus Torvalds"
+	"Talk is cheap. Show me the code.\n - Linus Torvalds"
+	"Those that can, do. Those that can't, complain.\n - Linus Torvalds"
 )
