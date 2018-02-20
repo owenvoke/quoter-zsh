8d53b2e3550217aa29db21ae40f8c6d58838450f
pxgamer <owzie123@gmail.com>
2018-02-20 16:46:25 +0000

    Add additional quote

--- src/default.zsh
+++ src/default.zsh
@@ -5,4 +5,5 @@ quoter_selection=(
 	"Those that can, do. Those that can't, complain.\n - Linus Torvalds"
 	"Things that are impossible just take longer.\n - Ian Hickson"
 	"When in doubt, leave it out.\n - Joshua Bloch"
+	"In theory, theory and practice are the same. In practice, they’re not.\n - Yoggi Berra"
 )
