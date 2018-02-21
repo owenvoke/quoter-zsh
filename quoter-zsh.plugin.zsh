5b0fc7b9ca97e4f0b3a0480878f79e9d4c2093cd
pxgamer <owzie123@gmail.com>
2018-02-21 08:55:35 +0000

    Add additional comments to the plugin

--- quoter-zsh.plugin.zsh
+++ quoter-zsh.plugin.zsh
@@ -3,7 +3,9 @@ if [[ ! "${QUOTER_BANK}" ]]; then
     QUOTER_BANK="default"
 fi
 
+# Generate the relative path to the quoter bank
 quoter_bank_file="$(dirname -- "$0")/src/${QUOTER_BANK}.zsh"
+
 # Check if the quote bank file exists
 if [[ -f "${quoter_bank_file}" ]]; then
     # Include the quote bank file
@@ -12,5 +14,7 @@ if [[ -f "${quoter_bank_file}" ]]; then
     # Output a random quote
     echo "${quoter_selection[RANDOM % ${#quoter_selection} + 1]}"
     echo
+
+    # Free up memory
     unset quoter_selection
 fi
